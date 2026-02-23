f1614c228c6b8ca39fe5a16301927d64bca82b66
leonie.oderso <leonie.buerzer@gmail.com>
2026-02-23 16:15:59 +0100

    Refactor LED control for multiple LEDs
    Updated LED control to manage multiple LEDs and added cleanup on exit.

--- 02_RaspberryPi/01_Master-V1/LED.py
+++ 02_RaspberryPi/01_Master-V1/LED.py
@@ -7,19 +7,40 @@ Device.pin_factory = LGPIOFactory()
 
 
 #Eingänge der LEDs
-LED_LG = LED(17)
-LED_LG.on()
+LED_rot = LED(17)
+LED_grün = LED(27)
+LED_w1 = LED(24)
+LED_w2 = LED(5)
+#LED_LG1 = LED()
+LED_LG2 = LED(26)
 
-time.sleep(3)
 
-try:
-    while True:
-        LED_LG.toggle()
-        time.sleep(2)
-        
+#LEDs anschalten
+LED_rot.on()
+LED_grün.on()
+LED_w1.on()
+LED_w2.on()
+#LED_LG1.on()
+LED_LG2.on()
+
+time.sleep(8)          #kurze Pause
+
+#try:
+    #while True:
+     
+
 except KeyboardInterrupt:
-    LED_LG.off()
-    LED_LG.close()
-    print("Die LED wurde ausgeschalten und der GPIO_PIN wieder freigegeben")
- 
+    LED_rot.off()                #LEDs werden ausgeschalten
+    LED_grün.off()
+    LED_w1.off()
+    LED_w2.off()
+    #LED_LG1.off()
+    LED_LG2.off()
 
+    LED_rot.close()              #Freigeben der LEDs
+    LED_grün.close()
+    LED_w1.close()
+    LED_w2.close()
+    #LED_LG1.close()
+    LED_LG2.close()
+    print("Die LED wurde ausgeschalten und der GPIO_PIN wieder freigegeben")
