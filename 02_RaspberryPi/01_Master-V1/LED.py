e76bb90e8b7a8e9273396809939eef1587f43539
TSFTHI <marek.zuehlke@airbus.com>
2026-02-23 10:27:58 +0100

    updated LED

--- 02_RaspberryPi/01_Master-V1/LED.py
+++ 02_RaspberryPi/01_Master-V1/LED.py
@@ -1,20 +1,27 @@
-from gpiozero import LED
-from time import sleep
+#from gpiozero import LED
+#from time import sleep
+
+from gpiozero import LED, Device 
+from gpiozero.pins.lgpio import LGPIOFactory
+import threading 
+import time
+
+Device.pin_factory = LGPIOFactory()
 
 #Eingänge der LEDs
 
-def main() -> None:
-LED_LG = LED(17)
-LED_LG.off()
 
-try:
-    while True:
-        led.toggle()
-        sleep(0.5)
+LED_LG = LED(17)
+LED_LG.on()
+time.sleep(5)
+#try:
+    #while True:
+        #led.toggle()
+        #sleep(0.5)
         
-except KeyboardInterrupt:
-    LED_LG.off()
-    LED_LG.close()
-    print("Die LED wurde ausgeschalten und der GPIO_PIN wieder freigegeben")
+#except KeyboardInterrupt:
+    #LED_LG.off()
+    #LED_LG.close()
+    #print("Die LED wurde ausgeschalten und der GPIO_PIN wieder freigegeben")
  
 
