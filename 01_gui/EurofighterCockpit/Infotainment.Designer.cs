78b407670526796c57826e9748ce4fc6cf0c7a73
Felix <felix.pannenbaecker@airbus.com>
2026-02-23 15:36:59 +0100

    set infotainment to full screen

--- 01_gui/EurofighterCockpit/Infotainment.Designer.cs
+++ 01_gui/EurofighterCockpit/Infotainment.Designer.cs
@@ -34,27 +34,30 @@
             this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(234, 794);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(358, 1080);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // p_content
             // 
             this.p_content.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.p_content.Dock = System.Windows.Forms.DockStyle.Top;
-            this.p_content.Location = new System.Drawing.Point(234, 0);
+            this.p_content.Location = new System.Drawing.Point(358, 0);
+            this.p_content.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.p_content.Name = "p_content";
-            this.p_content.Size = new System.Drawing.Size(1041, 653);
+            this.p_content.Size = new System.Drawing.Size(1562, 424);
             this.p_content.TabIndex = 1;
             // 
             // Infotainment
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1275, 794);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.Controls.Add(this.p_content);
             this.Controls.Add(this.flowLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Infotainment";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Infotainment";
