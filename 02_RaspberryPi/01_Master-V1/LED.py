2c9356539f884383878235aa6b08f390fb68ee52
leonie.oderso <leonie.buerzer@gmail.com>
2026-02-24 16:18:38 +0100

    alle LED's (fürs erste)

--- 02_RaspberryPi/01_Master-V1/LED.py
+++ 02_RaspberryPi/01_Master-V1/LED.py
@@ -11,7 +11,7 @@ LED_rot = LED(17)
 LED_grün = LED(27)
 LED_w1 = LED(24)
 LED_w2 = LED(5)
-#LED_LG1 = LED()
+LED_LG1 = LED(16)
 LED_LG2 = LED(26)
 
 
@@ -20,26 +20,31 @@ LED_rot.on()
 LED_grün.on()
 LED_w1.on()
 LED_w2.on()
-#LED_LG1.on()
+LED_LG1.on()
 LED_LG2.on()
 
-time.sleep(8)          #kontrolle ob alle LEDs leuchten
+time.sleep(3)          #kontrolle ob alle LEDs leuchten
 
 #alle LEDs aus
 LED_rot.off()
 LED_grün.off()
 LED_w1.off()
 LED_w2.off()
-#LED_LG1.off()
+LED_LG1.off()
 LED_LG2.off()
 
 
-time.sleep(4)          #kurze Pause
+time.sleep(2)          #kurze Pause
+
+#dauerhaftes leuchten von rot und grün
+LED_rot.on()            
+LED_grün.on()
 
 
- #dauerhaftes leuchten von rot und grün
-        LED_rot.on()            
-        LED_grün.on()
+#Landing Gear LEDs
+LED_LG1.on()
+LED_LG2.on()
+
 
 try:
     while True:
@@ -47,37 +52,43 @@ try:
         LED_w1.on()
         LED_w2.on()
 
-        time.sleep(0.5)
+        time.sleep(0.1)
 
         LED_w1.off()
         LED_w2.off()
 
-        time.sleep(1)
+        
+        time.sleep(0.1)
 
         LED_w1.on()
         LED_w2.on()
         
-        time.sleep(0.5)
+        time.sleep(0.1)
+
+        LED_w1.off()
+        LED_w2.off()
+
+
+        time.sleep(0.4)
 
 
 
-
-
-     
 
 except KeyboardInterrupt:
     LED_rot.off()                #LEDs werden ausgeschalten
     LED_grün.off()
     LED_w1.off()
     LED_w2.off()
-    #LED_LG1.off()
+    LED_LG1.off()
     LED_LG2.off()
 
     LED_rot.close()              #Freigeben der LEDs
     LED_grün.close()
     LED_w1.close()
     LED_w2.close()
-    #LED_LG1.close()
+    LED_LG1.close()
     LED_LG2.close()
+    
+    print("Die LEDs wurde ausgeschalten und der GPIO_PIN wieder freigegeben")
     print("Die LED wurde ausgeschalten und der GPIO_PIN wieder freigegeben")
  
