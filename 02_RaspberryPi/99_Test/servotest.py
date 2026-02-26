f5c9c20c2701d0b5adc30b1e62c9dce66cb2c002
TSFTHI <marek.zuehlke@airbus.com>
2026-02-26 14:00:14 +0100

    only for testing

--- 02_RaspberryPi/99_Test/servotest.py
+++ 02_RaspberryPi/99_Test/servotest.py
@@ -2,12 +2,12 @@ import time
 from adafruit_servokit import ServoKit
 
 # Initialisiere das ServoKit-Objekt mit der I2C-Adresse 0x41
-channel = 0
+channel = 12
 kit = ServoKit(channels=16, address=0x40,frequency=30)
 
 #LG = ServoKit(channels=16, address=0x41,frequency=30)
-for channel in range(16):
-            kit.servo[channel].set_pulse_width_range(1000,2000)
+#kit.servo[channel].set_pulse_width_range(500,2250)
+kit.servo[channel].set_pulse_width_range(1000,2000)
 # Setze die minimale und maximale Position des Servos
 min_position = 0  # Minimal Position
 max_position = 180  # Maximale Position
@@ -16,24 +16,24 @@ max_position = 180  # Maximale Position
 step_size = 10
 
 # Zeitverzögerung zwischen den Schritten (in Sekunden)
-delay = 6
+#delay = 0.003
 
-# Funktion zum Bewegen des Servos von min_position zu max_position
-def move_servo(angle):
-        print("moving")
-        kit.servo[channel].angle = angle
-        #LG.servo[0].fraction = 1
-        time.sleep(delay)
+# delay per step = time in seconds/stepscount
 
-try:
 
-    kit.servo[15].angle = 180
-    time.sleep(3)
-    kit.servo[15].angle = 0
-    time.sleep(3)
- 
 
-except KeyboardInterrupt:
-    # Wenn CTRL+C gedrückt wird, stoppe die Bewegung und setze den Servo auf 90 Grad
-    kit.servo[channel].angle = 0
+def delay(seconds, steps_difference):
+        delay = seconds/steps_difference
+        return delay
+
+#for angle in range(0,180):
+#    kit.servo[channel].angle = angle
+#    time.sleep(delay(10,180))
+#time.sleep(0.5)
+#kit.servo[channel].angle = 0
+#time.sleep(2)
+#kit.servo[channel].angle = None
+for i in range (10,1):
+    print(i)
+
 
