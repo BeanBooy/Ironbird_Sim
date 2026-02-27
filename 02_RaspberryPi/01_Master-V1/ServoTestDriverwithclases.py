a3053ef68f1cf95ec7909e51421d8039e168e30a
TSFTHI <marek.zuehlke@airbus.com>
2026-02-27 11:10:51 +0100

    nearly finished only need correct CD closing

--- 02_RaspberryPi/01_Master-V1/ServoTestDriverwithclases.py
+++ 02_RaspberryPi/01_Master-V1/ServoTestDriverwithclases.py
@@ -10,6 +10,11 @@ LG_IN = 0
 I2CSERVO = 0x40
 servodriver = ServoKit(channels=16, address=I2CSERVO, frequency=30)
 
+stop_test = threading.Event()
+executor = ThreadPoolExecutor(max_workers=1)
+current_test_future = None
+future_lock = threading.Lock()
+
 RCD = Servo(channel=0,idle=0)
 LCD = Servo(channel=1,idle=0)
 RU = Servo(channel=2,idle=90)
@@ -22,61 +27,74 @@ RF = Servo(channel=8,idle=128,inverted=False)
 AB = Servo(channel=9,idle=0)
 LG = Servo(channel=14,idle=0) # in reality channel 15. Object used only for checking state
 
-MODE = 2
-
 def safe_sleep(seconds):
     for i in range(seconds*50):
-        if MODE != 2:
-            break
+        if stop_test.is_set():
+            return
         time.sleep(0.02)
 
-def test_move(MODE):
-    for byte_value in range(0,256+1):
-        if MODE != 2:
-            return
-        RU.move(byte_value)
-        LC.move(byte_value)
-        RC.move(byte_value)
-        LO.move(byte_value)
-        RO.move(byte_value)
-        LF.move(byte_value)
-        RF.move(byte_value)
-        AB.move(byte_value)
-        if MODE != 2:
-            return
-        time.sleep(0.002) # for smoother movement
+def test_move():
+    for cycles in range(2):
+        if not stop_test.is_set():
+            for byte_value in range(0,256+1):
+                if stop_test.is_set():
+                    break
+                RU.move(byte_value)
+                LC.move(byte_value)
+                RC.move(byte_value)
+                LO.move(byte_value)
+                RO.move(byte_value)
+                LF.move(byte_value)
+                RF.move(byte_value)
+                AB.move(byte_value)
+                time.sleep(0.002) # for smoother movement
 
-    RCD.move(RCD.max_pos)
-    LCD.move(LCD.max_pos)
-    safe_sleep(1)
-    if RCD.current_pos == RCD.max_pos and LCD.current_pos == LCD.max_pos and MODE == 2:
-        servodriver.servo[15].fraction = LG_OUT
-        LG.current_pos = LG_OUT
-    elif MODE != 2:
-        return
+            RCD.move(RCD.max_pos)
+            LCD.move(LCD.max_pos)
+            safe_sleep(1)
+            if RCD.current_pos == RCD.max_pos and LCD.current_pos == LCD.max_pos and not stop_test.is_set():
+                servodriver.servo[15].fraction = LG_OUT
+                LG.current_pos = LG_OUT
 
-    for byte_value in range(256,0,-1):
-        if MODE != 2:
-            return
-        RU.move(byte_value)
-        LC.move(byte_value)
-        RC.move(byte_value)
-        LO.move(byte_value)
-        RO.move(byte_value)
-        LF.move(byte_value)
-        RF.move(byte_value)
-        AB.move(byte_value)
-        if MODE != 2:
-            return
-        time.sleep(0.005) # for smoother movement
-    #if RCD.current_pos is not RCD.idle and LCD.current_pos is not LCD.idle and MODE == 2:
-    servodriver.servo[15].fraction = LG_IN
-    LG.current_pos = LG_IN
-    safe_sleep(3)
-    if MODE != 2:
-            return
-    if LG.current_pos == LG_IN and MODE == 2:
-        RCD.move(RCD.idle)
-        LCD.move(LCD.idle)
-    
+            for byte_value in range(256,0,-1):
+                if stop_test.is_set():
+                    break
+                RU.move(byte_value)
+                LC.move(byte_value)
+                RC.move(byte_value)
+                LO.move(byte_value)
+                RO.move(byte_value)
+                LF.move(byte_value)
+                RF.move(byte_value)
+                AB.move(byte_value)
+                time.sleep(0.005) # for smoother movement
+            #if RCD.current_pos is not RCD.idle and LCD.current_pos is not LCD.idle and MODE == 2:
+            servodriver.servo[15].fraction = LG_IN
+            LG.current_pos = LG_IN
+            safe_sleep(3)
+            if LG.current_pos == LG_IN and not stop_test.is_set():
+                RCD.move(RCD.idle)
+                LCD.move(LCD.idle)
+
+def start_servo_test():
+    global current_test_future
+    with future_lock:
+        stop_test.set()
+        if current_test_future and not current_test_future.done():
+            current_test_future.cancel()
+
+        # new thread can be started
+        stop_test.clear()
+
+        # start new thread
+        current_test_future = executor.submit(test_move)
+
+def stop_servo_test():
+    global current_test_future
+
+    with future_lock:
+        stop_test.set()
+        if current_test_future:
+            current_test_future.cancel()
+
 #test_move(MODE)
