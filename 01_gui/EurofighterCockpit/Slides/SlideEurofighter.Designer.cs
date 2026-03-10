d2c0720f80db31a399c25cc3b98c7cb8b433aa1d
Felix <felix.pannenbaecker@airbus.com>
2026-03-10 07:50:59 +0100

    added EF2000 slide

--- 01_gui/EurofighterCockpit/Slides/SlideEurofighter.Designer.cs
+++ 01_gui/EurofighterCockpit/Slides/SlideEurofighter.Designer.cs
@@ -25,13 +25,27 @@
         /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SlideEurofighter));
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Leelawadee UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(124, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1366, 933);
+            this.label1.TabIndex = 2;
+            this.label1.Text = resources.GetString("label1.Text");
             // 
             // SlideEurofighter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.label1);
             this.Name = "SlideEurofighter";
             this.Size = new System.Drawing.Size(1600, 1080);
             this.ResumeLayout(false);
@@ -39,5 +53,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
